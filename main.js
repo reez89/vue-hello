7878b58a828510691a2829e200c9965f8eb5707c
Nicola R <nicola.rizzi89@hotmail.it>
2020-12-14 15:03:37 +0100

    Finished
    Message and Image taken from "DATA".

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Istruzioni:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data */
+
+let app = new Vue({
+    el: '#app', 
+    data:{
+        message: "Finally we meet, VueJS <3",
+        image: 'vue.png'
+    }
+});
